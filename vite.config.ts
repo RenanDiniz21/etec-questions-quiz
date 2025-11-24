2d471202e352d453adb7dafbcf9351f4a8c5cd72
Renan Diniz <119080451+RenanDiniz21@users.noreply.github.com>
2025-11-24 18:19:08 -0300

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/etec-questions-quiz/',
   build: {
     outDir: "dist"
   }
